5c48b5443c2c07633fc23b01d2cd7e0744e0da0e
Archangel <radishikerov@yahoo.com>
2017-07-26 14:31:34 +0300

    Implement animated progress.

--- SwiftRadialProgress/SwiftRadialProgress/ViewController.swift
+++ SwiftRadialProgress/SwiftRadialProgress/ViewController.swift
@@ -66,10 +66,61 @@ class ViewController: UIViewController {
         
         
     }
+    
+    func SetProgressAnimated() {
+        let circle = CAShapeLayer()
+        let centerPoint = CGPoint (x: vProgress.bounds.width / 2,
+                                   y: vProgress.bounds.width / 2);
+        let circleRadius : CGFloat = vProgress.bounds.width / 2 * 0.83;
+        
+        
+        let value = arc4random_uniform(MAX_VALUE)
+        let progress = CGFloat(value)/CGFloat(MAX_VALUE)
+        
+        let clockwise = true
+        let startAngle = 90
+        let endAngle = clockwise ? startAngle + 360 : startAngle - 360
+        
+        let startAngleRad = startAngle.degreesToRadians
+        let endAngleRad = endAngle.degreesToRadians
+        
+        let circlePath = UIBezierPath(arcCenter: centerPoint,
+                                      radius: circleRadius,
+                                      startAngle: CGFloat(startAngleRad),
+                                      endAngle: CGFloat(endAngleRad),
+                                      clockwise: clockwise );
 
-    @IBAction func onDraw(_ sender: Any) {
-        SetRandomProgress()
+        circle.path = circlePath.cgPath
+        circle.fillColor = UIColor.clear.cgColor
+        circle.strokeColor = UIColor.green.cgColor
+        circle.lineWidth = 20
+        circle.cornerRadius = 10
+        
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.duration = 2;
+        animation.isRemovedOnCompletion = false;
+        animation.fromValue = 0;
+        animation.toValue = 1;
+        animation.timingFunction = CAMediaTimingFunction.init(name: kCAMediaTimingFunctionEaseOut)
+        
+        circle.add(animation, forKey: "drawCircleAnimation")
+        vProgress.layer.sublayers?.removeAll()
+        // REMOVE FROM SUPERLAYER
+        vProgress.layer.addSublayer(circle)
     }
 
+    @IBAction func onDraw(_ sender: Any) {
+//        SetRandomProgress()
+         SetProgressAnimated()
+    }
+
+}
+
+extension Int {
+    var degreesToRadians: Double { return Double(self) * .pi / 180 }
+}
+extension FloatingPoint {
+    var degreesToRadians: Self { return self * .pi / 180 }
+    var radiansToDegrees: Self { return self * 180 / .pi }
 }
 
